372b1e455d6685a952602894dc1cac511bffe8e8
Jay <jpnappy@gmail.com>
2016-02-14 17:19:50 -0500

    local changes

--- app.rb
+++ app.rb
@@ -3,10 +3,12 @@ require 'pry'
 require 'httparty'
 
 get "/" do 
-   #2. load the index page
+   erb :index
 end 
 
 post "/results" do
+	binding.pry # look at what you have for 'params'
+
 	#3. store the subreddit value in a variable called "subreddit"
 
 	#4.send a get request to "http://reddit.com/r/#{subreddit}.json" using the ruby gem 
