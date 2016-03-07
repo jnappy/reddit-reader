b8050a162fcce09d7d93f7682ee58517ea28fbbb
Dev Eggers <deveggers@Brendans-MBP.home>
2016-03-06 20:54:22 -0500

    Functional Reddit Reader.

--- app.rb
+++ app.rb
@@ -2,18 +2,24 @@ require 'sinatra'
 require 'pry'
 require 'httparty'
 
-get "/" do 
+get "/" do
    # 2. load the index page
-end 
+	 erb :index
+end
 
 post "/results" do
 	#3. using the params hash, store the subreddit key value in a variable called "subreddit"
+	@subreddit = params
+  sub = params['sub']
 
-	#4.send a get request to "http://reddit.com/r/#{subreddit}.json" using the ruby gem 
+	#4.send a get request to "http://reddit.com/r/#{subreddit}.json" using the ruby gem
+
+  returned_results = HTTParty.get("http://reddit.com/r/#{sub}.json")
+
 	# HTTParty and store it in a variable called returned_results
 
 	@posts = returned_results['data']['children']
 
-	#5. load the results page 
+	#5. load the results page
+	erb :results
 end
-
